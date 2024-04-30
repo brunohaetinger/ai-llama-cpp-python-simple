8b16689caaba8da99fc23862bffc75fdcb5b3012
Bruno Haetinger <brunohaetinger@gmail.com>
2024-04-30 17:10:35 -0300

    Simple first model

---
+++ main.py
@@ -0,0 +1,48 @@
+from llama_cpp import Llama
+
+
+# GLOBAL VARIABLES
+my_model_path = "~/Documents/Projects/llama.cpp/models/Wizard-Vicuna-7B-Uncensored.Q4_0.gguf"
+CONTEXT_SIZE = 512
+
+
+# LOAD THE MODEL
+zephyr_model = Llama(model_path=my_model_path,
+                    n_ctx=CONTEXT_SIZE)
+
+
+
+def generate_text_from_prompt(user_prompt,
+                             max_tokens = 100,
+                             temperature = 0.3,
+                             top_p = 0.1,
+                             echo = True,
+                             stop = ["Q", "\n"]):
+
+
+
+
+   # Define the parameters
+   model_output = zephyr_model(
+       user_prompt,
+       max_tokens=max_tokens,
+       temperature=temperature,
+       top_p=top_p,
+       echo=echo,
+       stop=stop,
+   )
+
+
+   return model_output
+
+
+if __name__ == "__main__":
+
+
+   my_prompt = "What do you think about using blockchain in Tech companies?"
+
+
+   zephyr_model_response = generate_text_from_prompt(my_prompt)
+
+
+   print(zephyr_model_response)
